b80451ce652ee24aed4a66f49682122101b6f6cc
dangrt5 <randy.dang05@gmail.com>
2018-09-12 14:49:45 -0700

    super, and extends method of a class

--- app/index.js
+++ app/index.js
@@ -1,4 +1,12 @@
-import addToDom from "./addToDom";
-import * as math from "./math";
+// import addToDom from "./addToDom";
+// import * as math from "./math";
+//
+// addToDom("h1", `5*2 equals ${math.div(2,5)}`);
 
-addToDom("h1", `5*2 equals ${math.div(2,5)}`);
+
+import SuperPerson from "./person";
+
+const Bob = new SuperPerson("Bob", "breathing fire");
+
+Bob.greet();
+Bob.talk("Hey wassup");
